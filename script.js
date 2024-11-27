edca28e973e9956b8ca1db77083e1ef54364c335
sadgefujin <153971016+sadgefujin@users.noreply.github.com>
2024-11-27 11:44:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const cursor = document.querySelector(".cursor");
-window.addEventListener("mousemove", function(e){
-    cursor.style.left = (e.x-100)+"px";
-    cursor.style.top = (e.y-100)+"px";
-})
